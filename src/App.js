636525956bfc1d9aaad1bf25737ada50b3cb6e6e
ContainedChaos <95173963+ContainedChaos@users.noreply.github.com>
2023-02-10 15:47:48 +0600

    did navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import React from "react"; 
 
 function App() {
   return (
-    <div className="container">
-            Thrift Basket!
-        </div>
+    <>
+    <Navbar/>
+    </>
   );
 }
 
