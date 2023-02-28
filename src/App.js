f57d99190c454fe3ea1240887376738f6bf9d7cc
ContainedChaos <95173963+ContainedChaos@users.noreply.github.com>
2023-02-28 21:26:29 +0600

    dk

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import React from 'react';
+import Homepage from './components/homepage/homepage';
+import Login from './components/login/login';
+import Register from './components/register/register';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Homepage /> */}
+      <Login />
+      {/* <Register /> */}
+    </div>
+  );
+}
+
+export default App;
