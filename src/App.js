8d37e662f3f5e3906af14b15cb3887065a965a26
ContainedChaos <95173963+ContainedChaos@users.noreply.github.com>
2023-02-28 21:10:37 +0600

    routes (?)

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import React from 'react';
 import Homepage from './components/homepage/homepage';
 import Login from './components/login/login';
 import Register from './components/register/register';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
+
+  //const [ user, setLoginUser] = useState({})
+
   return (
-    <div className="App">
-      {/* <Homepage /> */}
-      <Login />
-      {/* <Register /> */}
-    </div>
+      <BrowserRouter>
+        {/* <Routes>
+          <Route path="/" element = {<Homepage />} />
+          <Route path="/login" element = {<Login />} />
+          <Route path="/register" element = {<Register />} />
+        </Routes> */}
+        <App />
+      </BrowserRouter>
   );
 }
 
